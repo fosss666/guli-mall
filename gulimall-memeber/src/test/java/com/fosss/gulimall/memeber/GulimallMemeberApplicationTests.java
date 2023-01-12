93bd4d39f527fc740339eea8d547160f9074bc2e
fo <1745179058@qq.com>
2023-01-12 14:49:35 +0800

    项目初始结构创建

---
+++ gulimall-memeber/src/test/java/com/fosss/gulimall/memeber/GulimallMemeberApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fosss.gulimall.memeber;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class GulimallMemeberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
