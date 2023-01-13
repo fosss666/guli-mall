6b9b16ef237f27932b5858e56a26ab85f98ab7e7
fo <1745179058@qq.com>
2023-01-13 16:12:44 +0800

    配置yml及测试crud

--- gulimall-product/src/test/java/com/fosss/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/fosss/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,69 @@
 package com.fosss.gulimall.product;
 
+import com.fosss.gulimall.product.entity.BrandEntity;
+import com.fosss.gulimall.product.service.BrandService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import java.util.Iterator;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
+    @Resource
+    private BrandService brandService;
 
+    /**
+     * 测试添加品牌
+     */
     @Test
-    void contextLoads() {
+    public void testInsert() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("中兴");
+        brandService.save(brandEntity);
     }
 
+
+    /**
+     * 测试查询品牌
+     */
+    @Test
+    public void testSelect() {
+        List<BrandEntity> list = brandService.list();
+        Iterator<BrandEntity> iterator = list.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+
+    /**
+     * 测试修改品牌
+     */
+    @Test
+    public void testUpdate() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setBrandId(1l);
+        brandEntity.setDescript("中华有为");
+        brandService.updateById(brandEntity);
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
