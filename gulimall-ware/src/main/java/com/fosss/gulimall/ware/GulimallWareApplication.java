9110bed2875f165b576c3f9455c3cab2f2c341a0
fo <1745179058@qq.com>
2023-01-13 20:21:55 +0800

    配置注册中心

--- gulimall-ware/src/main/java/com/fosss/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/fosss/gulimall/ware/GulimallWareApplication.java
@@ -2,7 +2,9 @@ package com.fosss.gulimall.ware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient//开启注册中心
 @SpringBootApplication
 public class GulimallWareApplication {
 
