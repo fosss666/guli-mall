97371a603314388fd5c9fd0d33a662d87a80169b
fo <1745179058@qq.com>
2023-01-14 19:49:51 +0800

    分布式组件-SpringCloud-Alibaba-nacos配置中心

--- gulimall-coupon/src/main/java/com/fosss/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/fosss/gulimall/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import com.fosss.gulimall.coupon.feign.BrandFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +28,7 @@ import javax.annotation.Resource;
  * @email 1745179058@qq.com
  * @date 2023-01-13 16:21:28
  */
+@RefreshScope//刷新并获取配置
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
@@ -33,6 +36,17 @@ public class CouponController {
     private CouponService couponService;
     @Resource
     private BrandFeignService brandFeignService;
+
+    //测试配置中心
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test/config")
+    public R testConfig(){
+        return R.ok().put("name",name).put("age",age);
+    }
 
     /**
      * 测试远程调用的测试接口
