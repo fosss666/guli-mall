a0b26985ef0925c27a41281b299059cd62b61215
fo <1745179058@qq.com>
2023-01-16 20:27:37 +0800

    商品服务-API-三级分类-删除-页面效果

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -1,5 +1,28 @@
 <template>
-  <el-tree :data="data" :props="defaultProps" @node-click="getCategories"></el-tree>
+  <el-tree :data="data" :props="defaultProps" @node-click="getCategories"
+           :expand-on-click-node="false"
+           node-key="catId"
+           show-checkbox>
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level<=2"
+            type="text"
+            size="mini"
+            @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length===0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+  </el-tree>
 </template>
 
 <script>
@@ -18,15 +41,23 @@ export default {
     this.getCategories()
   },
   methods: {
+    append(data) {
+      console.log(data)
+    },
+
+    remove(node, data) {
+      console.log("node", node)
+      console.log("data", data)
+    },
     //查询分类数据
     getCategories() {
       this.$http({
         url: this.$http.adornUrl('/product/category/list/tree'),
         method: 'get'
-      }).then(res=>{
+      }).then(res => {
         // console.log("res:",res)
         // console.log("res.data",res.data)
-        this.data=res.data.data
+        this.data = res.data.data
 
       })
       // categoryApi.getCategories().then(res=>{
@@ -39,5 +70,12 @@ export default {
 </script>
 
 <style scoped>
-
+.custom-tree-node {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  padding-right: 8px;
+}
 </style>
