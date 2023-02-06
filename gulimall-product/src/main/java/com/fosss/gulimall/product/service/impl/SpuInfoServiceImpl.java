1c4718ed7732a05334e5d53fd706fec27d24e065
fo <1745179058@qq.com>
2023-02-06 17:50:29 +0800

    商品服务-API-新增商品-保存spu-6.1保存sku的基本信息 pms_sku_info

--- gulimall-product/src/main/java/com/fosss/gulimall/product/service/impl/SpuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/fosss/gulimall/product/service/impl/SpuInfoServiceImpl.java
@@ -1,10 +1,10 @@
 package com.fosss.gulimall.product.service.impl;
 
-import com.fosss.gulimall.product.entity.ProductAttrValueEntity;
-import com.fosss.gulimall.product.entity.SpuImagesEntity;
-import com.fosss.gulimall.product.entity.SpuInfoDescEntity;
+import com.fosss.gulimall.product.entity.*;
 import com.fosss.gulimall.product.service.*;
 import com.fosss.gulimall.product.vo.BaseAttrs;
+import com.fosss.gulimall.product.vo.Images;
+import com.fosss.gulimall.product.vo.Skus;
 import com.fosss.gulimall.product.vo.SpuSaveVo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -23,7 +23,6 @@ import com.fosss.common.utils.PageUtils;
 import com.fosss.common.utils.Query;
 
 import com.fosss.gulimall.product.dao.SpuInfoDao;
-import com.fosss.gulimall.product.entity.SpuInfoEntity;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
@@ -40,6 +39,8 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
     private ProductAttrValueService productAttrValueService;
     @Resource
     private AttrService attrService;
+    @Resource
+    private SkuInfoService skuInfoService;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -96,9 +97,33 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
         }).collect(Collectors.toList());
         productAttrValueService.saveBatch(productAttrValueEntities);
 
-        //5.保存spu的积分信息 gulimall_sms->sms_spu_bounds
+        //TODO 5.保存spu的积分信息 gulimall_sms->sms_spu_bounds
+
         //6.保存spu的sku信息
-        //6.1保存sku的基本信息 pms_sku_info
+        List<Skus> skus = spuSaveVo.getSkus();
+        skus.forEach((sku)->{
+            //6.1保存sku的基本信息 pms_sku_info
+            SkuInfoEntity skuInfoEntity = new SkuInfoEntity();
+            skuInfoEntity.setSkuName(sku.getSkuName());
+            skuInfoEntity.setSkuTitle(sku.getSkuTitle());
+            skuInfoEntity.setPrice(sku.getPrice());
+            skuInfoEntity.setSkuSubtitle(sku.getSkuSubtitle());
+            skuInfoEntity.setBrandId(spuSaveVo.getBrandId());
+            skuInfoEntity.setCatalogId(spuSaveVo.getCatalogId());
+            skuInfoEntity.setSaleCount(0L);
+            //获取默认图片
+            String defaultImage="";
+            List<Images> skuImages = sku.getImages();
+            for (Images skuImage : skuImages) {
+                if(skuImage.getDefaultImg()==1){
+                    defaultImage = skuImage.getImgUrl();
+                }
+            }
+            skuInfoEntity.setSkuDefaultImg(defaultImage);
+            skuInfoService.save(skuInfoEntity);
+
+        });
+
         //6.2保存sku的销售属性 pms_sku_sale_attr_value
         //6.3保存sku的图片信息 pms_sku_images
         //6.4保存sku的优惠、满减等信息；gulimall_sms->sms_sku_ladder\sms_sku_full_reduction\sms_member_price
