03d2bb7edfd460ec13baaa088c7c0bdab95f1e1d
fo <1745179058@qq.com>
2023-03-05 14:48:38 +0800

    缓存-分布式锁-redisson-测试读写锁

--- gulimall-product/src/main/java/com/fosss/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/fosss/gulimall/product/web/IndexController.java
@@ -6,7 +6,9 @@ import com.fosss.gulimall.product.service.CategoryService;
 import com.fosss.gulimall.product.vo.Catelog2Vo;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
+import org.redisson.api.RReadWriteLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author fosss
@@ -28,6 +31,52 @@ public class IndexController {
     private CategoryService categoryService;
     @Resource
     private RedissonClient redissonClient;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 测试读写锁
+     * 读写锁能够保证读到的数据一定是最新的，写锁是排他锁（互斥锁），多个写锁应排队进行，读锁是共享锁，能够同时读，写锁未释放，读锁就一直等待
+     */
+    @ResponseBody
+    @GetMapping("/write")
+    public String write() {
+        RReadWriteLock rReadWriteLock = redissonClient.getReadWriteLock("ReadWriteLock");
+        //获取读锁
+        RLock rLock = rReadWriteLock.writeLock();
+        //上锁
+        rLock.lock();
+        try {
+            stringRedisTemplate.opsForValue().set("writeValue", UUID.randomUUID().toString());
+            Thread.sleep(30000);
+        } catch (Exception e) {
+
+        } finally {
+            //释放写锁
+            rLock.unlock();
+        }
+        return "写成功";
+    }
+
+    @ResponseBody
+    @GetMapping("/read")
+    public String read() {
+        RReadWriteLock rReadWriteLock = redissonClient.getReadWriteLock("ReadWriteLock");
+        //获取读锁
+        RLock rLock = rReadWriteLock.readLock();
+        //上锁
+        rLock.lock();
+        String writeValue = "";
+        try {
+            writeValue = stringRedisTemplate.opsForValue().get("writeValue");
+        } catch (Exception e) {
+
+        } finally {
+            //释放读锁
+            rLock.unlock();
+        }
+        return "读到的数据：" + writeValue;
+    }
 
     /**
      * 测试redisson分布式锁
