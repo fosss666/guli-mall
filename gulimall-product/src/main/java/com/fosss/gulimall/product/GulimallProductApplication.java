9d53fb168f2226bfdcac1428229a030904e68119
fo <1745179058@qq.com>
2023-03-08 15:17:36 +0800

    缓存-spring-cache使用场景

--- gulimall-product/src/main/java/com/fosss/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/fosss/gulimall/product/GulimallProductApplication.java
@@ -8,20 +8,18 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
- *
  * 读多写少读数据直接用SpringCache,写数据可以加读写锁，读多写多直接查数据库
+ * 常规数据（读多写少，即时性、一致性要求不高的数据）完全可以使用springCache的注解来使用缓存   写模式只要有缓存过期时间就足够了
+ * 其他数据：特殊处理   比如高并发的用redisson
+ * <p>
  * 配置spring-cache
  *
  * @Cacheable: Triggers cache population.触发器缓存填充
- *
  * @CacheEvict: Triggers cache eviction.触发缓存逐出
- *
  * @CachePut: Updates the cache without interfering with the method execution.在不干扰方法执行的情况下更新缓存
- *
  * @Caching: Regroups multiple cache operations to be applied on a method.组合以上多个缓存操作
- *
  * @CacheConfig: Shares some common cache-related settings at class-level.在类级别共享一些与缓存相关的常见设置
- *
+ * <p>
  * 整合springCache:
  * 1.导入坐标spring-boot-starter-cache
  * 2.配置缓存类型为redis
