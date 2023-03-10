6381bb8b85a923f661d20b1c6195962f41f6b89d
fo <1745179058@qq.com>
2023-03-10 10:50:11 +0800

    商城业务-检索服务-调整页面跳转

--- gulimall-search/src/main/java/com/fosss/gulimall/search/controller/SearchController.java
+++ gulimall-search/src/main/java/com/fosss/gulimall/search/controller/SearchController.java
@@ -1,12 +1,7 @@
 package com.fosss.gulimall.search.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.naming.directory.SearchResult;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author: fosss
@@ -16,9 +11,9 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 public class SearchController {
-    @GetMapping(value = "/search.html")
+    @GetMapping(value = "/list.html")
     public String listPage() {
 
-        return "search";
+        return "list";
     }
 }
