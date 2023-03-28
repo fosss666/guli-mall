1e1f9d649708698ed709938767467ada91b78921
fo <1745179058@qq.com>
2023-03-28 19:34:03 +0800

    商城业务-异步-CompletableFuture-线程串行化

--- gulimall-search/src/main/java/com/fosss/gulimall/search/thread/CompletableFutureTest.java
+++ gulimall-search/src/main/java/com/fosss/gulimall/search/thread/CompletableFutureTest.java
@@ -22,24 +22,60 @@ public class CompletableFutureTest {
         //}, service);
 
         //启动线程任务-supplyAsync   有返回值
+        //CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+        //    System.out.println("当前线程：" + Thread.currentThread().getId());
+        //    int res = 666 / 0;
+        //    return res;
+        //}, service).whenCompleteAsync((res, exception) -> {
+        //    //该方法能够获得结果和感知异常
+        //    System.out.println("打印结果：" + res + " 抛出异常：" + exception);
+        //}, service).exceptionally((e) -> {
+        //    //该方法能够感知异常并返回默认结果
+        //    System.out.println("异常：" + e);
+        //    return 6;
+        //}).handleAsync((res, exception) -> {
+        //    //该方法能够获得结果、感知异常、返回结果，相当于whenCompleteAsync + exceptionally
+        //    System.out.println("结果：" + res + " 异常：" + exception);
+        //    return 66;
+        //}, service);
+        //
+        //System.out.println("返回值" + future.get());
+
+        /**
+         * 线程串行化
+         */
+
+        //1
+        //CompletableFuture.supplyAsync(() -> {
+        //    System.out.println("当前线程：" + Thread.currentThread().getId());
+        //    int res = 10 / 2;
+        //    return res;
+        //}, service).thenRunAsync(() -> {
+        //    //thenRunAsync 无法获得结果，无返回值
+        //    System.out.println("线程2启动了");
+        //}, service);
+
+        //2
+        //CompletableFuture.supplyAsync(() -> {
+        //    System.out.println("当前线程：" + Thread.currentThread().getId());
+        //    int res = 10 / 2;
+        //    return res;
+        //}, service).thenAcceptAsync((res) -> {
+        //    //thenAcceptAsync 可以获得结果，无返回值
+        //    System.out.println("线程2启动了,结果为："+res);
+        //}, service);
+
+        //3
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
             System.out.println("当前线程：" + Thread.currentThread().getId());
-            int res = 666 / 0;
+            int res = 10 / 2;
             return res;
-        }, service).whenCompleteAsync((res, exception) -> {
-            //该方法能够获得结果和感知异常
-            System.out.println("打印结果：" + res + " 抛出异常：" + exception);
-        }, service).exceptionally((e) -> {
-            //该方法能够感知异常并返回默认结果
-            System.out.println("异常：" + e);
-            return 6;
-        }).handleAsync((res, exception) -> {
-            //该方法能够获得结果、感知异常、返回结果，相当于whenCompleteAsync + exceptionally
-            System.out.println("结果：" + res + " 异常：" + exception);
-            return 66;
+        }, service).thenApplyAsync((res) -> {
+            //thenApplyAsync 可以获得结果，有返回值
+            System.out.println("线程2启动了,结果为：" + res);
+            return res;
         }, service);
-
-        System.out.println("返回值" + future.get());
+        System.out.println("返回：" + future.get());
     }
 }
 
