5400a9ca15f36e39afd3fbc5cd17666eb229d5b7
fo <1745179058@qq.com>
2023-05-29 14:59:46 +0800

    商城业务-消息队列-springboot整合rabbitmq

--- gulimall-order/src/main/java/com/fosss/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/fosss/gulimall/order/GulimallOrderApplication.java
@@ -1,10 +1,20 @@
 package com.fosss.gulimall.order;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * 整合rabbitmq:
+ * 1.引入amqp环境坐标
+ * 2.配置rabbitmq环境
+ * 3.开启对rabbitmq的支持 @EnableRabbit
+ * 4.使用RabbitAdmin创建exchange、queue等，进行收发消息等操作
+ */
+
+@EnableRabbit
 @EnableDiscoveryClient//开启注册中心
 @SpringBootApplication
 @ComponentScan(basePackages = "com.fosss")
--- gulimall-order/src/test/java/com/fosss/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/fosss/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,13 +1,41 @@
 package com.fosss.gulimall.order;
 
 import org.junit.Test;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class GulimallOrderApplicationTests {
+import javax.annotation.Resource;
 
+@SpringBootTest
+public class GulimallOrderApplicationTests {
+    @Resource
+    private RabbitAdmin rabbitAdmin;
+
+    /**
+     * 测试rabbitmq
+     */
     @Test
-    void contextLoads() {
+    void testRabbitMq() {
+
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
